e7a65e03091eb5b4d4a85d374b87cd0307a2a75b
Roberto arce <roberto_arce_@hotmail.com>
2020-09-10 11:59:43 +0200

    changes on pp

--- src/ft_conv_pp.c
+++ src/ft_conv_pp.c
@@ -6,7 +6,7 @@
 /*   By: titorium <rarce@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 17:57:25 by titorium          #+#    #+#             */
-/*   Updated: 2020/09/10 11:43:40 by titorium         ###   ########.fr       */
+/*   Updated: 2020/09/10 11:57:59 by titorium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,27 @@ static int	ft_putpp()
 }
 
 
-int	ft_pp_conv(t_flags flag)
+int	ft_pp_conv(t_flags flag, int zeros)
 {
 	int		counter;
 	int		spaces;
 	int		word_len;
+	
 
 	counter = 0;
 	word_len = 1;
 	if (flag.precision < word_len && flag.point == 1)
 		word_len = flag.precision;
 	spaces = 0;
+	if (flag.width > word_len && flag.point == 0 && flag.zero == 1)
+		zeros = flag.width - word_len ;
+	if (flag.point == 1 && flag.precision > word_len)
+		zeros = flag.precision - word_len;
+
 	if (flag.width > word_len)
 		spaces = flag.width - word_len;
 	if (flag.negative == 1)
-		counter = ft_putpp()+ ft_s(spaces) + 1;
+		counter = ft_z(zeros)+ft_putpp()+ ft_s(spaces) + 1;
 	else
 		counter = ft_s(spaces) + ft_putpp() + 1;
 	while (spaces > counter++)
